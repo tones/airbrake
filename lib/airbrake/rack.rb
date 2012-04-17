c6dffed53627fb4545f6f8a869df743fd0c2df0b
Tim Jones <tim@upworthy.com>
2012-04-17 12:08:07 -0700

    .

--- lib/airbrake/rack.rb
+++ lib/airbrake/rack.rb
@@ -28,6 +28,7 @@ module Airbrake
       begin
         response = @app.call(env)
       rescue Exception => raised
+        puts "EXCEPTION"
         error_id = Airbrake.notify_or_ignore(raised, :rack_env => env)
         env['airbrake.error_id'] = error_id
         raise
